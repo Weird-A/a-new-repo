5d2fc81ca5afd1e33efc9bb26cb3cc9954713657
Weird-A <akintolaabdulhaq05@gmail.com>
2025-08-22 13:06:04 +0100

    Update creating files

--- creating.js
+++ creating.js
@@ -6,9 +6,24 @@ buttonEl.addEventListener('click', function(e){
     let newPlayer= inputEl.value;
 
     let listItem = document.createElement('li');
+
+    let delBtn = document.createElement('button');
+    delBtn.innerText = 'Delete';
+    delBtn.style.marginLeft = '10px';
+
+
     listItem.innerText = newPlayer;
+
+    listItem.append('newPlayer');
+    listItem.appendChild('delBtn');
+
     listEl.appendChild(listItem);
+
     inputEl.value = '';
+
+    delBtn.addEventListener('click', (e) => {
+        listItem.remove();
+    })
 })
 
 let commentButton = document.getElementsByClassName('comment')[0];
@@ -28,4 +43,33 @@ commentButton.addEventListener('click', function (e) {
     comment.innerText = newComment;
     commentUser.appendChild(comment);
     inputUser2.value = '';
+})
+
+let beforeEndBtn = document.querySelector('.before-end');
+let afterBeginBtn = document.querySelector('.after-begin');
+let box = document.querySelector('div.box');
+
+// let remove = document.querySelector('#remove');
+// remove.addEventListener('click', (e) => )
+
+function createBox (){
+   let div = document.createElement('div');
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.backgroundColor = 'white';
+
+    return div;
+}
+beforeEndBtn.addEventListener('click', (e) => {
+    let newDiv = createBox();
+
+    newDiv.style.border = '1px solid green'
+
+    box.insertAdjacentElement('beforeend', newDiv);
+})
+afterBeginBtn.addEventListener('click', (e) => {
+    let newDiv = createBox();
+
+    newDiv.style.border = '1px solid red'
+    box.insertAdjacentElement('afterbegin', newDiv);
 })
